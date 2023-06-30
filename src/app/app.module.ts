6837b8c057af43da5fb4b06c08dcb9024478e9ac
Krishanu <krishanu58@gmail.com>
2023-07-01 00:19:08 +0530

    footer-1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SlideContentComponent } from './components/slide-content/slide-content.component';
 import { ContentListComponent } from './components/content-list/content-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { Footer1Component } from './components/footer1/footer1.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     HomeComponent,
     SlideContentComponent,
     ContentListComponent,
-    NavbarComponent
+    NavbarComponent,
+    Footer1Component
   ],
   imports: [
     BrowserModule,
